0c6f697a965a953142b9061fffa0d601df66cfd0
madcoder1000000 <39967871+madcoder1000000@users.noreply.github.com>
2018-07-31 14:29:20 -0400

    Add files via upload

---
+++ CommandGodOn.java
@@ -0,0 +1,21 @@
+package com.meeku.tutorialPlugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CommandGodOn implements CommandExecutor {
+
+    // This method is called, when somebody uses our command
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            player.setInvulnerable(true);
+        }
+
+        // If the player (or console) uses our command correct, we can return true
+        return true;
+    }
+}
